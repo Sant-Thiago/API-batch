b90499f6ea547999dcc69235d5c0accd49ea5403
Sant-Thiago <thiagovieirab2b@gmail.com>
2024-06-27 21:45:41 -0300

    [FINISH] comando catch feito

--- Automations/catch.bat
+++ Automations/catch.bat
@@ -2,25 +2,34 @@
 
 setlocal enabledelayedexpansion
 
-set tempFile=tempFile%time%.txt
+:start
+    :: pega os dois ultimos valores da string do time HH:MM:SS
+    set id=%time:~6, 2%
+    set tempFile=tempFile%id%.txt
 
-dir /s /b "%1*" > %tempFile%
+    dir /s /b "%1*" > %tempFile%
 
-call firstLine %tempFile%
+    call :firstLine %tempFile%
+    set file=%res%
+    
+    call :read %file%
+    set resFinal=!res!
 
-set file=%res%
+    endlocal & echo %res%
+goto :eof
 
-del %tempFile%
+:read
+    set file=%1
+    set lines=
 
-@REM call read %file%
-echo %file%
+    for /f "delims=" %%l in (%file%) do (
+        set lines=!lines!%%l^&echo.
+    )
 
-
-@REM set file=%res%
-@REM del tempFile.txt
-
-@REM :: retira a palavra 'echo.', que é interpretada, e uma quebra de linha
-@REM set "lines=!lines:~0, -6!"
+    :: retira a palavra 'echo.', que é interpretada, e uma quebra de linha
+    set lines=!lines:~0, -6!
+    set res=!lines!
+goto :eof
 
 :firstLine
     set file=%1
@@ -34,12 +43,10 @@ echo %file%
         )
     )
 
-    set "res=!lines:~0, -6!"
+    set res=%lines%
+    del %tempFile%
 
 goto :eof
-
-
-endlocal
 
 :: usebackq: permite que o nome do arquivo seja cercado por aspas duplas, tratando o argumento como um arquivo de texto
 
